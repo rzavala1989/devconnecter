1964ce103de151a5f0c4a0b84163e3e9e1e1e858
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2019-07-05 00:58:13 -0700

    added jwt auth

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://rzavala1989:illmatic774@ds011725.mlab.com:11725/dev-connecter'
+    mongoURI: 'mongodb://rzavala1989:illmatic774@ds011725.mlab.com:11725/dev-connecter',
+    secretOrkey: 'secret'
 }
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -7,6 +9,11 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
+// Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+//DB Config
 const db = require ('./config/keys').mongoURI;
 
 //connect to MongoDB
@@ -16,7 +23,11 @@ mongoose
     .then(() => console.log("MongoDB Connected!"))
     .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send("Hello World"));
+// Passport middleware 
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 //USE routes
 
