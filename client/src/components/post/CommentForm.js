d9c4b4458a707298e074db56b8b0007831a4a9c8
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2020-01-29 21:29:52 -0800

    gitignore

---
+++ client/src/components/post/CommentForm.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
+import { addPost, addComment } from "../../actions/postActions";
+
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+      errors: {}
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.errors) {
+      this.setState({ errors: newProps.errors });
+    }
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const { postId } = this.props;
+    const { user } = this.props.auth;
+    const newComment = {
+      text: this.state.text,
+      name: user.name,
+      avatar: user.avatar
+    };
+    this.props.addComment(postId, newComment);
+    this.setState({ text: "" });
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  render() {
+    //define errors
+    const { errors } = this.state;
+
+    return (
+      <div className="post-form mb-3">
+        <div className="card card-info">
+          <div className="card-header bg-info text-white">
+            Reply to this post
+          </div>
+          <div className="card-body">
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                <TextAreaFieldGroup
+                  placeholder="Leave some feedback, please..."
+                  name="text"
+                  value={this.state.text}
+                  onChange={this.onChange}
+                  error={errors.text}
+                />
+              </div>
+              <button type="submit" className="btn btn-dark">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+CommentForm.propTypes = {
+  addPost: PropTypes.func.isRequired,
+  addComment: PropTypes.func.isRequired,
+
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+  postId: PropTypes.string.isRequired
+};
+
+const mapStateToProps = state => ({
+  errors: state.errors,
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { addPost, addComment }
+)(CommentForm);
--- server.js
+++ server.js
@@ -1,40 +1,40 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
 // Body Parser middleware
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //DB Config
-const db = require ('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 //connect to MongoDB
 
 mongoose
-    .connect(db)
-    .then(() => console.log("MongoDB Connected!"))
-    .catch(err => console.log(err));
+  .connect(db)
+  .then(() => console.log("MongoDB Connected!"))
+  .catch(err => console.log(err));
 
-// Passport middleware 
+// Passport middleware
 app.use(passport.initialize());
 
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 //USE routes
 
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on pot ${port}`));
+app.listen(port, () => console.log(`Running on port ${port}`));
