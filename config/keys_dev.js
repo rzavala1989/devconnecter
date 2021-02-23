5d3412c1ef14d1d4d894e0f972dfb207e7bdc3a1
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2021-02-22 23:06:59 -0800

    migration

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:
+    // "mongodb://rzavala1989:illmatic774@ds011725.mlab.com:11725/dev-connecter"
+    "mongodb+srv://rzavala1989:illmatic774@dev-connecter.12mqv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
