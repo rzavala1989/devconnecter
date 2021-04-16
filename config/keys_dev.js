7ae9113e32bc54e89d69d788e553e4178d102ba9
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2021-04-16 01:59:45 -0700

    migrate

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,6 +1,5 @@
 module.exports = {
   mongoURI:
-    // "mongodb://rzavala1989:illmatic774@ds011725.mlab.com:11725/dev-connecter"
     "mongodb://rzavala1989:illmatic774@dev-connecter.12mqv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   secretOrKey: "secret",
 };
